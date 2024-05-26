7398d068a9fac80db9b286da558dca75f956a30d
hoangluccos <94107943+hoangluccos@users.noreply.github.com>
2024-05-26 22:17:45 +0700

    commit

--- src/main/java/com/learning/configruation/TestPass.java
+++ src/main/java/com/learning/configruation/TestPass.java
@@ -4,6 +4,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class TestPass {
     public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123123123123"));
+        System.out.println(new BCryptPasswordEncoder().encode("123"));
     }
 }
