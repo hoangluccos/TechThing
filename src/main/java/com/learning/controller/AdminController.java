a0c9d9cdb7c01cdc4699dbc8e4da57e33afe2e3f
hoangluccos <94107943+hoangluccos@users.noreply.github.com>
2024-05-26 22:28:25 +0700

    com1

--- src/main/java/com/learning/controller/AdminController.java
+++ src/main/java/com/learning/controller/AdminController.java
@@ -1,19 +1,37 @@
 package com.learning.controller;
 
 import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.learning.service.UserService;
 
 @Controller
 public class AdminController {
-	
+	@Autowired
+	UserService userService;
 	@GetMapping("/admin")
 	public String adminHome(HttpSession session)
 	{
 		UserDetails userDetails = (UserDetails) session.getAttribute("userDetails");
 		System.out.println(userDetails.getUsername());
-		return "admin/home";
+		return "admin/homeAdmin";
+	}
+	@GetMapping("/admin/user")
+	public String listUser(Model model)
+	{
+		model.addAttribute("users", userService.findAll());
+		return "admin/user";
+
+	}
+	@GetMapping("/admin/user/delete/{id}")
+	public String deleteUser(@PathVariable String id) {
+		userService.deleteById(id);
+		return "redirect:/admin/user";
 	}
 }
